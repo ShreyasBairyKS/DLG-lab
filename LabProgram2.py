981af8e63a81f2ff2f9106f9dc64ee1bf1271fd0
Shreyas Bairy K S <shreyasbairyks@gmail.com>
2026-01-13 11:49:45 +0530

    Update LabProgram2.py

--- LabProgram2.py
+++ LabProgram2.py
@@ -9,7 +9,7 @@ y = np.array([[0], [1], [1], [0]])
 model = Sequential()
 model.add(Dense(8, input_dim=2, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-
+   
 model.compile(loss='binary_crossentropy', optimizer=SGD(learning_rate=0.1), metrics=['accuracy'])
 
 model.fit(X, y, epochs=10000, verbose=0)
